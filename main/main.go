288bfbcbfa0ba40287b30616829dd4f692142596
samara <samara.ribeiro@m4u.com.br>
2021-09-03 01:20:54 -0300

    Add register and func report

--- main/main.go
+++ main/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/start", report)
+
+	http.ListenAndServe(":8080", router)
+}
+
+type register struct {
+	id uuid.UUID
+	name string
+	age int64
+	state string
+	report string
+	
+}
+
+func report(w http.ResponseWriter, r *http.Request){
+	
+}
