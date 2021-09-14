b1c9a690b7b08bbbc76a5312d99667b849f14c88
samara <samara.ribeiro@m4u.com.br>
2021-09-14 16:34:04 -0300

    Documents

--- main/main.go
+++ main/main.go
@@ -21,11 +21,25 @@ type register struct {
 	report string
 }
 
-var registers = []register{
+//var registers = []register{
 
-	{cpf: "8444096358", name: "Ted", age:  35, state: "Rio de Janeiro", report: "violência doméstica"},
+//	{cpf: "8444096358", name: "Ted", age:  35, state: "Rio de Janeiro", report: "violência doméstica"},
+//	{cpf: "9547078545", name: "Robin", age:  35, state: "São Paulo", report: "roubo"},
+//Poderia usar Map aqui também? (Estudar e analisar LISTA E MAP para procedimento
+//}
 
+
+var register1 = register{
+
+	("15092785958"),("Joana"),28, "Paraná", "Assalto",
 }
+
+var register2 = register{
+
+	("894556215415"),("Fred"),18, "Minas Gerais", "Maus tratos",
+}
+
+var registers[] = register(register1,register2)
 
 func report(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(registers)
